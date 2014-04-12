44be9de980331403f6b32fcd80de6b8b4f8459e0
Richard Hill <xrkhill@gmail.com>
2014-04-11 23:10:32 -0600

    Rails generated app with RSpec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Products::Application.config.secret_token = 'c512a5ad6e1668ab780c09817a43a974e9638d4854442966e5040cc34d326c4369ce804dba5bec3f8b81758a9c45d65f1801177bd70c87c0cc8456946d8a5fb3'
