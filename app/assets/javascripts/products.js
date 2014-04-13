7d2becc564e6dbc26e95c0f10c2b40d667c8ee94
Richard Hill <xrkhill@gmail.com>
2014-04-12 21:09:27 -0600

    Add Backbone.js gem

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,13 @@
+window.Products = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Products.initialize();
+});
