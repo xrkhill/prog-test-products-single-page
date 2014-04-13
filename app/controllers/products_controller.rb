fa066364fd391c2ad15f93baec024f9eaf15f43a
Richard Hill <xrkhill@gmail.com>
2014-04-12 20:41:03 -0600

    Add controller actions for CRUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,31 @@
 class ProductsController < ApplicationController
-  respond_to :json
+  respond_to :json, :html
 
   def index
     @products = Product.all
-    respond_with(@products)
+    respond_with(@product)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    respond_with(@product)
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    @product.save
+    respond_with(@product)
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update_attributes(params[:product])
+    respond_with(@product)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    respond_with(@product)
   end
 end
