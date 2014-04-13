0fb79266dd1f0d7cda69aff91168f3a69cbbc65d
Richard Hill <xrkhill@gmail.com>
2014-04-12 20:47:59 -0600

    Add some exception handling for JSON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActionController::RoutingError, with: :routing_error
+  rescue_from StandardError, with: :standard_error
+
+  protected
+
+  def routing_error(exception)
+    respond_with({ error_message: exception.message }, status: :not_found)
+  end
+
+  def standard_error(exception)
+    respond_with({ error_message: exception.message }, status: :internal_server_error)
+  end
 end
