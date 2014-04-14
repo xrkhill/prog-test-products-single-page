c547ad7807ef10e690ca26ccb67ebb0b45634ea4
Richard Hill <xrkhill@gmail.com>
2014-04-14 02:23:10 -0600

    Heroku asset config fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,4 @@
 //= require backbone
 //= require backbone.marionette
 //= require products
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
 //= require_tree .
